edef6d6c3cc6a7e8d3d143225ee8bafc0f52ae1f
Gingerfridge <75426240+Gingerfridge@users.noreply.github.com>
2020-12-03 17:41:42 +0000

    adding docunments for analysis

--- Folder_NumDens_50000_50000_Job_47_a/SLD_profiles/SLD_profiles_Rascal/Project/SLD_4_Rascal_with_slab_initiliser.m
+++ Folder_NumDens_50000_50000_Job_47_a/SLD_profiles/SLD_profiles_Rascal/Project/SLD_4_Rascal_with_slab_initiliser.m
@@ -7,9 +7,9 @@ function [output] = SLD_4_Rascal_with_slab_initiliser(params,bulk_in,bulk_out,co
 surface_switch = params(1);
 
 if surface_switch == 1
-    [xx,yy] = SLD_sim_and_slab(params,bulk_in,bulk_out,contrast,surface_switch)    
+    [xx,yy] = SLD_sim_and_slab_no_rough(params,bulk_in,bulk_out,contrast,surface_switch)    
 elseif surface_switch == 2
-    [xx,yy] = SLD_sim_and_slab(params,bulk_in,bulk_out,contrast,surface_switch)    
+    [xx,yy] = SLD_sim_and_slab_no_rough(params,bulk_in,bulk_out,contrast,surface_switch)    
 
 %only simulation SLD
 else
