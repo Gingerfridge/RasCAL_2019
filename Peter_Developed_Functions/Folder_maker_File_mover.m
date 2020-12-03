5d21c4a9da1a4c4e9f48f2210bb0dfbdad1d9193
Gingerfridge <75426240+Gingerfridge@users.noreply.github.com>
2020-12-03 12:04:52 +0000

    Adding files from simulation Job_47

--- Peter_Developed_Functions/Folder_maker_File_mover.m
+++ Peter_Developed_Functions/Folder_maker_File_mover.m
@@ -1,5 +1,5 @@
 function Folder_maker_File_mover(folder_label)
-folder_label_ext = string(strjoin(["Folder_",folder_label],''));
+folder_label_ext = string(strjoin(["C:\Users\mbcx4ph5\Dropbox (The University of Manchester)\PhD\RasCAL_2019\Folder_",folder_label],''));
 filenames = files()
 folder_label_star = string(strjoin([folder_label,"*"],''));
 movefile( folder_label_star, folder_label_ext );
