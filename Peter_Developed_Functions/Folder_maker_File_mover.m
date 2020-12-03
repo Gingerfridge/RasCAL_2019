9dbc66adb3e2fa7ef719bd08662a9f943c0263a7
Gingerfridge <75426240+Gingerfridge@users.noreply.github.com>
2020-12-03 12:01:37 +0000

    acually restored to normal

--- Peter_Developed_Functions/Folder_maker_File_mover.m
+++ Peter_Developed_Functions/Folder_maker_File_mover.m
@@ -1,5 +1,5 @@
 function Folder_maker_File_mover(folder_label)
-folder_label_ext = string(strjoin([,folder_label],''));
+folder_label_ext = string(strjoin(["Folder_",folder_label],''));
 filenames = files()
 folder_label_star = string(strjoin([folder_label,"*"],''));
 movefile( folder_label_star, folder_label_ext );
