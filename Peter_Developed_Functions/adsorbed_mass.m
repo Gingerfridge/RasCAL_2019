0a8718a67bb4a7d53f76b78fe764e3f6e2c7cd54
Gingerfridge <75426240+Gingerfridge@users.noreply.github.com>
2020-12-04 11:24:03 +0000

    Update adsorbed_mass.m
    Trying to get it to auto output adsorbed masss, will add surface area poer molecule

--- Peter_Developed_Functions/adsorbed_mass.m
+++ Peter_Developed_Functions/adsorbed_mass.m
@@ -1,9 +1,9 @@
 function [Total_AA_Mass,Total_AA_Mass_layer,Total_AA_Adsorbed]  = adsorbed_mass(OUTPUT_M,Hyd_Factor,box_x,box_y)
 nucleaon_mass = 1.6605*10.^-27;
-%kg/nm^2 -> mg/m^2 
+%kg/nm^2 -> mg/m^2
 units = 10.^24;
-Total_AA_Mass_layer = sum(OUTPUT_M,2)./Hyd_Factor*nucleaon_mass;
+Total_AA_Mass_layer = sum(OUTPUT_M,2)./Hyd_Factor*nucleaon_mass
 
-Total_AA_Mass = sum(Total_AA_Mass_layer);
+Total_AA_Mass = sum(Total_AA_Mass_layer)
 
-Total_AA_Adsorbed = sum(Total_AA_Mass_layer)/(box_x*box_y)*units;
+Total_AA_Adsorbed = sum(Total_AA_Mass_layer)/(box_x*box_y)*units
